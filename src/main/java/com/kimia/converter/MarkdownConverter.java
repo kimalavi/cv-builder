470cf7f1c5683b63a9a9f4acb382b355494552cc
Kimia Alavi <43290829+kimalavi@users.noreply.github.com>
2025-09-13 21:01:24 +0330

    refactor: remove unused Spire imports from MarkdownConverter
    Remove several unused imports from MarkdownConverter.java that referenced
    Spire doc classes (Document, DocumentObject, Section Paragraph, TextRange,
    formatting and collection types). The converter now delegates directly to
    SpireConverter.convert and does not require those types; removing them
    cleans up warnings and simplifies imports. No functional change.

--- src/main/java/com/kimia/converter/MarkdownConverter.java
+++ src/main/java/com/kimia/converter/MarkdownConverter.java
@@ -1,24 +1,10 @@
 package com.kimia.converter;
 
 import com.kimia.converter.spire.SpireConverter;
-import com.spire.doc.Document;
-import com.spire.doc.DocumentObject;
 import com.spire.doc.FileFormat;
-import com.spire.doc.Section;
-import com.spire.doc.collections.DocumentObjectCollection;
-import com.spire.doc.collections.ParagraphCollection;
-import com.spire.doc.collections.SectionCollection;
-import com.spire.doc.documents.Paragraph;
-import com.spire.doc.fields.TextRange;
-import com.spire.doc.formatting.CharacterFormat;
-import com.spire.doc.formatting.ParagraphFormat;
 
 public class MarkdownConverter {
     public static void convert(String mdFileName, String wordFileName, FileFormat fileFormat) {
         SpireConverter.convert(mdFileName, wordFileName, fileFormat);
     }
-
-
-
-
 }
